28b6141e68b440c970fe5e30c574754b68a88dd5
rexcorp.id <71690257+rexcorp01@users.noreply.github.com>
2020-12-23 01:09:16 +0700

    Update db.config.js

--- backend/src/db/db.config.js
+++ backend/src/db/db.config.js
@@ -1,13 +1,13 @@
 // module.exports = {
 //   development: {
-//     "username": "admin",
-//     "password": "admin_pass",
-//     "database": "nodejs_backend_db",
-//     "host": "127.0.0.1",
+//     "username": "tcfpvjzhcjetln",
+//     "password": "bf333f2fde7f34504b7eb24748e628c48fedccd61959c1649e7381cc48aecca1",
+//     "database": "d9jl2u5gmj17gq",
+//     "host": "ec2-3-211-167-220.compute-1.amazonaws.com",
 //     "dialect": "postgres"
 //   },
 //   production: {
-//     "use_env_variable": "DATABASE_URL",
+//     "use_env_variable": "postgres://tcfpvjzhcjetln:bf333f2fde7f34504b7eb24748e628c48fedccd61959c1649e7381cc48aecca1@ec2-3-211-167-220.compute-1.amazonaws.com:5432/d9jl2u5gmj17gq",
 //     "dialect": "postgres"
 //   }
 // };
